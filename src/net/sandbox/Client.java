d032d4387aa14d1faebc5bff348d587ea94219ef
pmaass53 <pippinmaass53@gmail.com>
2025-11-20 16:10:53 +0000

    Update client

--- src/net/sandbox/Client.java
+++ src/net/sandbox/Client.java
@@ -1,7 +1,13 @@
 package net.sandbox;
 
+import java.util.logging.Logger;
+
+import net.sandbox.window.Window;
+
 public class Client {
+    private static final Logger LOGGER = Logger.getLogger(Client.class.getName());
     public static void main(String[] args) {
-        System.out.println("Initializing Client...");
+        LOGGER.info("Initializing Client...");
+        Window.bindInstance(new Window());
     }
 }
