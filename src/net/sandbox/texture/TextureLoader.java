6760fa7e9655039d2bdb194ad05ad9571390e75c
pmaass53 <pippinmaass53@gmail.com>
2025-11-21 13:29:45 +0000

    Upload from Codespace

--- src/net/sandbox/texture/TextureLoader.java
+++ src/net/sandbox/texture/TextureLoader.java
@@ -2,16 +2,28 @@ package net.sandbox.texture;
 
 import java.util.HashMap;
 import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.logging.Logger;
 
 public class TextureLoader {
+    private static final Logger LOGGER = Logger.getLogger(Canvas.class.getName());
     public static String TEXTURES_RELATIVE_PATH = "./resources/textures/";
     public static HashMap<String, Image> cache = new HashMap<String, Image>();
     public static Image getTexture(String key) {
-        if (cache.containsKey(key)) {
-            return cache.get(key);
-        } else {
-            // load texture
-            // add to cache
+        if (!cache.containsKey(key)) {
+            cache.put(key, readFromFile(key));
         }
+        return cache.get(key);
+    }
+    public static Image readFromFile(String fileName) {
+        Image img;
+        try {
+            // read image
+            img = ImageIO.read(new File("./resources/textures/" + fileName + ".jpg"));
+        } catch (Exception e) {
+            LOGGER.severe("Error Loading Image from File: " + e.getMessage(), e);
+        }
+        return img;
     }
 }
